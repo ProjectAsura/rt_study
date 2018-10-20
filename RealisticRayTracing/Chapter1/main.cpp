d11f7e1eeccd8c2ef28a9cbf5006b32a3b4d9e6a
Pocol <project.asura@gmail.com>
2018-10-20 17:11:32 +0900

    追加。

--- RealisticRayTracing/Chapter1/main.cpp
+++ RealisticRayTracing/Chapter1/main.cpp
@@ -1,6 +1,52 @@
 
+#include "image.h"
+#include "vector3.h"
+#include "onb.h"
+#include "rgb.h"
+#include "rng.h"
+#include "dynarray.h"
+#include <cassert>
 
 int main(int argc, char** argv)
 {
+    {
+        DynArray<int> test;
+        test.append(1);
+        test.append(2);
+        test.append(3);
+
+        assert(test.length() == 3);
+
+        for(auto i=0; i<test.length(); ++i)
+        {
+            printf_s("%d\n", test[i]);
+        }
+        assert(test[0] == 1);
+        assert(test[1] == 2);
+        assert(test[2] == 3);
+        //assert(test.arraySize == 4);
+
+        test.append(4);
+        test.append(5);
+
+        assert(test[3] == 4);
+        assert(test[4] == 5);
+        //assert(test.arraySize == 8);
+
+        test.truncate();
+        assert(test[0] == 1);
+        assert(test[1] == 2);
+        assert(test[2] == 3);
+        assert(test[3] == 4);
+        assert(test[4] == 5);
+        //assert(test.arraySize == 5);
+        //assert(test.nData == 5);
+    }
+
+    {
+        Image image(512, 256, rgb(1.0f, 0.0f, 0.0f));
+        image.writePPM("test.ppm");
+    }
+
     return 0;
 }
--- RealisticRayTracing/Chapter1/rng.h
+++ RealisticRayTracing/Chapter1/rng.h
@@ -1,1 +1,25 @@
+// RNG.h
 #pragma once
+
+class RNG
+{
+public:
+    unsigned long long  seed;
+    unsigned long long  mult;
+    unsigned long long  llong_max;
+    float               float_max;
+
+    RNG(unsigned long long _seed = 7564231ULL)
+    : seed(_seed)
+    {
+        mult = 62089911ULL;
+        llong_max = 4294967295ULL;
+        float_max = 4294967295.0f;
+    }
+
+    float operator() ()
+    {
+        seed = mult * seed;
+        return float(seed % llong_max) / float_max;
+    }
+};
