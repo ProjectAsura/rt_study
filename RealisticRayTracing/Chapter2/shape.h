2f8bbfc248ab7b0e25982d3a0e4b943d1b67343b
Pocol <project.asura@gmail.com>
2018-10-20 18:02:33 +0900

    実装追加。

---
+++ RealisticRayTracing/Chapter2/shape.h
@@ -0,0 +1,20 @@
+﻿// Shape.h
+#pragma once
+
+#include "ray.h"
+#include "vector3.h"
+#include "rgb.h"
+
+struct HitRecord
+{
+    float   t;
+    Vector3 normal;
+    rgb     color;
+};
+
+class Shape
+{
+public:
+    virtual bool hit(const Ray& ray, float tmin, float max, float time, HitRecord& record) const = 0;
+    virtual bool shadowHit(const Ray& ray, float tmin, float tmax, float time) const = 0;
+};
