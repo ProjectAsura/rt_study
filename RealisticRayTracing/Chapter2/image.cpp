d2a9054d0f7bbddd71dc4e0843f2b6adcd0edd36
Pocol <project.asura@gmail.com>
2018-10-20 17:31:06 +0900

    Chapter2ベースファイルを追加。

---
+++ RealisticRayTracing/Chapter2/image.cpp
@@ -0,0 +1,145 @@
+﻿// Image.cc
+
+#include "image.h"
+#include <iostream>
+
+Image::Image(int width, int height)
+: nx(width)
+, ny(height)
+{
+    // allocate memory for raster
+    raster = new rgb* [nx];
+    for(auto i=0; i<nx; ++i)
+    { raster[i] = new rgb[ny]; }
+}
+
+Image::Image(int width, int height, rgb background)
+: nx(width)
+, ny(height)
+{
+    // allocate memory for raster
+    raster = new rgb* [nx];
+    for(auto i=0; i<nx; ++i)
+    {
+        raster[i] = new rgb[ny];
+        // assign 'background& to each element
+        for(auto j=0; j<ny; ++j)
+        { raster[i][j] = background; }
+    }
+}
+
+bool Image::set(int x, int y, const rgb& color)
+{
+    // check for out of bounds error
+    if (0 > x || x > nx) return false;
+    if (0 > y || y > ny) return false;
+
+    raster[x][y] = color;
+    return true;
+}
+
+void Image::gammaCorrect(float gamma)
+{
+    auto power = 1.0f / gamma;
+    for(auto i=0; i<nx; ++i)
+    {
+        for(auto j=0; j<ny; ++j)
+        {
+            auto temp = raster[i][j];
+            raster[i][j] = rgb(
+                pow(temp.r(), power),
+                pow(temp.g(), power),
+                pow(temp.b(), power));
+        }
+    }
+}
+
+void Image::writePPM(std::ostream& out)
+{
+    // output header
+    out << "P3\n";
+    out << nx << ' ' << ny << '\n';
+    out << "255\n";
+
+    for(auto i=ny-1; i>=0; i--)
+    {
+        for(auto j=0; j<nx; ++j)
+        {
+            auto r = static_cast<unsigned int>(256 * raster[j][i].r());
+            auto g = static_cast<unsigned int>(256 * raster[j][i].g());
+            auto b = static_cast<unsigned int>(256 * raster[j][i].b());
+
+            if (r > 255) r = 255;
+            if (g > 255) g = 255;
+            if (b > 255) b = 255;
+
+            out << r << " " << g << " " << b << " ";
+        }
+    }
+}
+
+bool Image::writePPM(const char* path)
+{
+    // open stream to file.
+    std::ofstream out;
+    out.open(path);
+    if (!out.is_open())
+    {
+        std::cerr << "ERROR -- Couldn't open file \'" << path << "\'.\n";
+        return false;
+    }
+
+    writePPM(out);
+    out.close();
+    return true;
+}
+
+bool Image::readPPM(const char* path)
+{
+    // open stream to file
+    std::ifstream in;
+    in.open(path);
+    if (!in.is_open())
+    {
+        std::cerr << "ERROR -- Couldn't open file \'" << path << "\'.\n";
+        return false;
+    }
+
+    char ch, type;
+    char r, g, b;
+    int  cols, rows, count;
+
+    // read in header info
+    in.get(ch);
+    in.get(type);
+    in >> cols >> rows >> count;
+
+    nx = cols;
+    ny = rows;
+
+    // allocate raster
+    raster = new rgb* [nx];
+    for(auto i=0; i<nx; ++i)
+    { raster[i] = new rgb[ny]; }
+
+    // clean up newlines
+    in.get(ch);
+
+    // store PPM pixel values in raster
+    for(auto i=ny-1; i>=0; i--)
+    {
+        for(auto j=0; j<nx; ++j)
+        {
+            in.get(r);
+            in.get(g);
+            in.get(b);
+
+            raster[j][i] = rgb(
+                float(unsigned char(r) / 255.0),
+                float(unsigned char(g) / 255.0),
+                float(unsigned char(b) / 255.0));
+        }
+    }
+
+    return true;
+}
---
+++ RealisticRayTracing/Chapter2/onb.cpp
@@ -0,0 +1,107 @@
+﻿// ONB.cc
+#include "onb.h"
+
+namespace {
+
+constexpr float ONB_EPSILON = 0.01f;
+
+} // namespace
+
+
+void ONB::initFromU(const Vector3& u)
+{
+    Vector3 n(1.0f, 0.0f, 0.0f);
+    Vector3 m(0.0f, 1.0f, 0.0f);
+
+    U = unitVector(u);
+    V = cross(U, n);
+    if (V.length() < ONB_EPSILON)
+    { V = cross(U, m); }
+    W = cross(U, V);
+}
+
+void ONB::initFromV(const Vector3& v)
+{
+    Vector3 n(1.0f, 0.0f, 0.0f);
+    Vector3 m(0.0f, 1.0f, 0.0f);
+
+    V = unitVector(v);
+    U = cross(V, n);
+    if (U.length() < ONB_EPSILON)
+    { U = cross(V, m); }
+    W = cross(U, V);
+}
+
+void ONB::initFromW(const Vector3& w)
+{
+    Vector3 n(1.0f, 0.0f, 0.0f);
+    Vector3 m(0.0f, 1.0f, 0.0f);
+
+    W = unitVector(w);
+    U = cross(W, n);
+    if (U.length() < ONB_EPSILON)
+    { U = cross(W, m); }
+    V = cross(W, U);
+}
+
+void ONB::initFromUV(const Vector3& u, const Vector3& v)
+{
+    U = unitVector(u);
+    W = unitVector(cross(u, v));
+    V = cross(W, U);
+}
+
+void ONB::initFromVU(const Vector3& v, const Vector3& u)
+{
+    V = unitVector(v);
+    W = unitVector(cross(u, v));
+    U = cross(V, W);
+}
+
+void ONB::initFromUW(const Vector3& u, const Vector3& w)
+{
+    U = unitVector(u);
+    V = unitVector(cross(w, u));
+    W = cross(U, V);
+}
+
+void ONB::initFromWU(const Vector3& w, const Vector3& u)
+{
+    W = unitVector(w);
+    V = unitVector(cross(w, u));
+    U = cross(V, W);
+}
+
+void ONB::initFromVW(const Vector3& v, const Vector3& w)
+{
+    V = unitVector(v);
+    U = unitVector(cross(v, w));
+    W = cross(U, V);
+}
+
+void ONB::initFromWV(const Vector3& w, const Vector3& v)
+{
+    W = unitVector(w);
+    U = unitVector(cross(v, w));
+    V = cross(W, U);
+}
+
+bool operator == (const ONB& lhs, const ONB& rhs)
+{ return (lhs.u() == rhs.u() && lhs.v() == rhs.v() && lhs.w() == rhs.w()); }
+
+std::istream& operator >> (std::istream& is, ONB& value)
+{
+    Vector3 u, v, w;
+    is >> u >> v >> w;
+    value.initFromUV(u, v);
+    return is;
+}
+
+std::ostream& operator << (std::ostream& os, const ONB& value)
+{
+    os << value.u() << "\n"
+       << value.v() << "\n"
+       << value.w() << "\n";
+    return os;
+}
+
